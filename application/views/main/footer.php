5fa061bf8e5b8c68f0b9b0388a3cbb92cbc20c03
Walid SIA <walidsiadev@outlook.fr>
2018-06-20 01:56:51 +0200

    :sparkles: Module/prof_js

--- application/views/main/footer.php
+++ application/views/main/footer.php
@@ -3,5 +3,12 @@
 <script src="<?php echo base_url('/public/js/bootstrap.min.js'); ?>"></script>
 <script src="<?php echo base_url('/public/js/jquery.metisMenu.js'); ?>"></script>
 <script src="<?php echo base_url('/public/js/custom.js'); ?>"></script>
+    <!--import jquery confirme-->
+    <script src="<?php echo base_url('/public/js/jquery.js'); ?>"></script>
+    <link href="<?php echo base_url('/public/css/jquery-confirm.css'); ?>" rel="stylesheet" />
+    <script src="<?php echo base_url('/public/js/jquery.confirm.js'); ?>"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.0/jquery-confirm.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.0/jquery-confirm.min.js"></script>
+    <script src="<?php echo base_url('/public/js/prof/prof.js'); ?>"></script>
 </body>
 </html>
